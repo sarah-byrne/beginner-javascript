b0e5257301fb91c18e1a5b1d30a729912692f07c
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2020-01-18 22:20:00 +0000

    completed to end of video 36 - modals, tabs and intersection observer

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,14 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions')
+const button = document.querySelector('.accept')
+
+const handleObservation = o => {
+  if (o[0].isIntersecting) {
+    if (button.disabled) {
+      button.disabled = false
+      observer.unobserve(terms.lastElementChild)
+    }
+  }
+}
+
+const observer = new IntersectionObserver(handleObservation)
+observer.observe(terms.lastElementChild)
--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,26 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+console.log('ya ya wes we get it.. IT WORKS!')
+
+const tabs = document.querySelector('.tabs')
+const tabButtons = tabs.querySelectorAll('[role = "tab"]')
+const tabPanels = tabs.querySelectorAll('[role = "tabpanel"]')
+
+const handleTabClick = e => {
+  tabButtons.forEach(button => {
+    button.setAttribute('aria-selected', false)
+  })
+
+  tabPanels.forEach(panel => {
+    panel.hidden = true
+  })
+
+  const selectedTab = e.currentTarget
+  selectedTab.setAttribute('aria-selected', true)
+
+  const tabPanelToShow = tabs.querySelector(
+    `[aria-labelledby = ${selectedTab.id}]`
+  )
+  tabPanelToShow.hidden = false
+}
+tabButtons.forEach(button => {
+  button.addEventListener('click', handleTabClick)
+})
